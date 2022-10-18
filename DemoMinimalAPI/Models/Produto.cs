8336e6bcb98c595d1e2f2d28b9c2e14ae880b9c7
Mauro Rossi <maurorossi@maurorossi.com>
2022-10-18 10:09:59 -0300

    Iniciando repo do projeto MinimalAPI

---
+++ DemoMinimalAPI/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoMinimalAPI.Models
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ DemoMinimalAPI/Program.cs
@@ -0,0 +1,30 @@
+using DemoMinimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MinimalContextDb>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/produto", async (
+            MinimalContextDb context) =>
+        await context.Produtos.ToListAsync())
+    .WithName("GetProduto")
+    .WithTags("Produto");
+
+
+app.Run();
+
