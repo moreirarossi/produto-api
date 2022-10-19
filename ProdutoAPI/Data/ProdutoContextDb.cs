bd71808293c4e49dd6a70ee40472b06668574930
Mauro Rossi <maurorossi@maurorossi.com>
2022-10-19 14:21:47 -0300

    Alteração do nome da solução

---
+++ ProdutoAPI/Data/ProdutoContextDb.cs
@@ -0,0 +1,33 @@
+﻿using ProdutoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProdutoAPI.Data
+{
+    public class ProdutoContextDb: DbContext
+    {
+        public ProdutoContextDb(DbContextOptions<ProdutoContextDb> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Codigo)
+                .IsRequired()
+                .HasColumnType("char(10)");
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(max)");
+
+            modelBuilder.Entity<Produto>()
+                .ToTable("Produtos");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ProdutoAPI/Migrations/20221018120445_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoMinimalAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Codigo = table.Column<string>(type: "char(10)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
